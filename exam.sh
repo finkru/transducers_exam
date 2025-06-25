4886496214d7a2d7555933cc3bcc073043a1750a
finkru <likeawaldy@gmail.com>
2025-06-25 20:42:14 +0300

    raw

--- exam.sh
+++ exam.sh
@@ -2,6 +2,8 @@ lexd task.lexd | hfst-txt2fst -o lexd.hfst
 hfst-twolc -q task.twol -o twol.hfst
 hfst-compose-intersect lexd.hfst twol.hfst -o task.hfst
 
+curl -s https://raw.githubusercontent.com/agricolamz/2025_morphological_transducers/refs/heads/main/task_tests/for_test_exam_02.txt > for_test_exam_02.txt
+
 hfst-fst2strings task.hfst > forms.txt
 
 grep -xvf forms.txt for_test_exam_02.txt > missing_forms.txt
