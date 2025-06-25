f149160c5f69794cb255ac93adbd1867f186f4c6
finkru <likeawaldy@gmail.com>
2025-06-25 20:37:50 +0300

    exam

--- exam.sh
+++ exam.sh
@@ -2,8 +2,6 @@ lexd task.lexd | hfst-txt2fst -o lexd.hfst
 hfst-twolc -q task.twol -o twol.hfst
 hfst-compose-intersect lexd.hfst twol.hfst -o task.hfst
 
-curl -s https://raw.githubusercontent.com/agricolhw4-finkru/exam/exam.ipynb hw4-finkru/exam/exam.sh hw4-finkru/exam/task.lexd hw4-finkru/exam/task.twolamz/2025_morphological_transducers/refs/heads/main/task_tests/for_test_exam_02.txt > for_test_exam_02.txt
-
 hfst-fst2strings task.hfst > forms.txt
 
 grep -xvf forms.txt for_test_exam_02.txt > missing_forms.txt
